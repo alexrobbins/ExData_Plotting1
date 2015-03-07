3c2c75e63f642ec662d42654d537d541f6a7db74
Alex Robbins <alexander.j.robbins@gmail.com>
2015-03-07 16:16:10 -0600

    Update readme

--- getData.R
+++ getData.R
@@ -1,4 +1,6 @@
 # This file downloads and unzips the source data.
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-              "household_power_consumption.zip", method="curl")
-unzip("household_power_consumption.zip")
+getData <- function() {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                "household_power_consumption.zip", method="curl")
+  unzip("household_power_consumption.zip")
+}
